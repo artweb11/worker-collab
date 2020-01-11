879849c9f8cd1b060c4184c2451149abc4fb931d
Sebastian Bratu <artweb11@gmail.com>
2020-01-11 08:02:26 +0200

    react app6

--- api/index.js
+++ api/index.js
@@ -4,10 +4,9 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .use((req, res) => res.end('Hello world') )
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
