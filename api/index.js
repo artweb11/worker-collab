5f4d9cc35b4c56b710c7a15a1281fbe5382cf679
Sebastian Bratu <artweb11@gmail.com>
2020-01-11 07:57:43 +0200

    react app6

--- api/index.js
+++ api/index.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const app = express();
-
+var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.end('Hello world');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen( port );
-
-io.on('connection', function(socket){
-    console.log('a user connected');
-
-    socket.on('message', function(msg){
-        socket.broadcast( msg );
-    })
+http.listen( port, function(){
+  console.log('listening on *:3000');
 });
